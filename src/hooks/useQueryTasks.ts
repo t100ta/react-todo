9d04999aebd03c35d810b552553e1b8f51f399fb
t100ta <cjx.open_d@me.com>
2024-12-01 21:49:43 +0900

    init

---
+++ src/hooks/useQueryTasks.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { useError } from './useError'
+import { Task } from '../types'
+import { useQuery } from '@tanstack/react-query'
+
+export const useQueryTasks = () => {
+  const { switchErrorHandling } = useError()
+  const getTasks = async () => {
+    const { data } = await axios.get<Task[]>(
+      `${process.env.REACT_APP_API_URL}/tasks`,
+      { withCredentials: true }
+    )
+    return data
+  }
+  return useQuery<Task[], Error>({
+    queryKey: ['tasks'],
+    queryFn: getTasks,
+    staleTime: Infinity,
+    onError: (err: any) => {
+      if (err.response.data.message) {
+        switchErrorHandling(err.response.data.message)
+      } else {
+        switchErrorHandling(err.response.data)
+      }
+    },
+  })
+}
